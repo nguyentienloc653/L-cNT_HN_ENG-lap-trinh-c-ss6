ec7aca7f2bcb12f78f5a287ed24bebaee7f94f85
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-27 02:56:50 +0700

    upload ss6-3

---
+++ Bài 3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int matkhau=23102006,nhapmatkhau;
+	printf("Nhap mat khau :\n");
+	scanf("%d",&nhapmatkhau);
+	if(nhapmatkhau!=matkhau){
+		printf("Ban da nhap sai mat khau\n");
+	}else{
+		printf("Nhap mat dung\n");
+	}
+	return 0;
+	
+}
